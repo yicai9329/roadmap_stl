e0932fb01c9d4b0b0fd7c005626932d52811c836
yicai9329 <18081878178@163.com>
2017-11-29 17:31:12 +0800

    list success

--- src/components/newReportLive.vue
+++ src/components/newReportLive.vue
@@ -80,26 +80,32 @@
 				<div id="reportTtileTwo" class="row">
 					<div id="reportTitleTwoAddOrDelete" class="col s2  lime lighten-4">
 						<span class="indigo-text darken-3">增加文本域</span>&nbsp;<a href="#!"><i class="material-icons">add</i></a> <br />
-						<span class="indigo-text darken-3">增加图片</span>&nbsp;<a href="#!" v-on:click="toggle"><i class="material-icons">add</i></a><br />
+						<span class="indigo-text darken-3">增加图片</span>&nbsp;<a class="waves-effect waves-light btn-small modal-trigger" data-target="modal1">Modal</a><br />
 						<span class="indigo-text darken-3">删掉文本域</span>&nbsp;<a href="#!"><i class="material-icons">cancel</i></a> <br />
-						<span class="indigo-text darken-3">删掉图片</span>&nbsp;<a href="#!" v-on:click="closeTapTarget"><i class="material-icons">cancel</i></a>
+						<span class="indigo-text darken-3">删掉图片</span>&nbsp;<a href="#!"><i class="material-icons">cancel</i></a>
 					</div>
 					<div id="reportTextAndImage" class="col s10  deep-orange lighten-5">
 
 					</div>
 				</div> 
 			</div>
-			<a id="menu" class="waves-effect waves-light btn btn-floating" ><i class="material-icons">menu</i></a>
-			<div class="tap-target" data-activates="menu">
-				<div id="graphSelectArea" class="tap-target-content">
-					<form id="graphSelectForm" action="#">
-						<template v-for="graphItem in graphItems">
-							<span v-bind:key="graphItem.key">
-								<input class="with-gap" name="group1" value="graphItem.code" type="radio" id="graphItem.key">
-								<label for="graphItem.key">{{graphItem.title}}</label>
-							</span><br />
-						</template>
-					</form>
+			<div id="modal1" class="modal bottom-sheet">
+				<div class="modal-content">
+					<h4>图片选择列表</h4>
+					<div id="graphSelectArea">
+						<form id="graphSelectForm" action="#">
+							<template v-for="graphItem in graphItems">
+								<span :key="graphItem.key">
+									<input class="with-gap" name="group1" :value="graphItem.code" type="radio" :id="graphItem.key" v-model="selectedGraphCode">
+									<label :for="graphItem.key">{{graphItem.title}}</label>
+								</span><br />
+							</template>
+						</form>
+						<p>你选择的图号是: {{selectedGraphCode}}</p>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">确认</a>
 				</div>
 			</div>
 		</div>
@@ -113,6 +119,7 @@
 			return {
 				reportCategory: '月报',
 				reportTheme: '宏观经济研究月报2017年第10期',
+				selectedGraphCode: 0,
 				graphItems: [
 				{
 					key: "graph2",
@@ -133,6 +140,81 @@
 					key: "graph6",
 					code: 6,
 					title: "国内GDP季度增速图"
+				},
+				{
+					key:"graph7",
+					code: 7,
+					title: "国内PPI和CPI指数图"
+				},
+				{
+					key:"graph8",
+					code: 8,
+					title: "国内PMI指数图"
+				},
+				{
+					key:"graph9",
+					code: 9,
+					title: "国内社会消费品零售额"
+				},
+				{
+					key: "graph10",
+					code: 10,
+					title: "国内固定资产投资图"
+				},
+				{
+					key: "graph11",
+					code: 11,
+					title: "房地产投资增速与房价指数"
+				},
+				{
+					key: "graph12",
+					code: 12,
+					title: "国内进出口额"
+				},
+				{
+					key: "graph13",
+					code: 13,
+					title: "国内财政收入增速"
+				},
+				{
+					key: "graph15",
+					code: 15,
+					title: "国内货币投放量"
+				},
+				{
+					key: "graph16",
+					code: 16,
+					title: "国内市场利率"
+				},
+				{
+					key: "graph17",
+					code: 17,
+					title: "大宗商品价格指数"
+				},
+				{
+					key: "graph18",
+					code: 18,
+					title: "制造业分行业工业增加值增速"
+				},
+				{
+					key: "graph19",
+					code: 19,
+					title: "制造业分行业固定资产投资增速"
+				},
+				{
+					key: "graph20",
+					code: 20,
+					title: "同行业拆借隔夜利率"
+				},
+				{
+					key: "graph21",
+					code: 21,
+					title: "两市股指与两市融余"
+				},
+				{
+					key: "graph22",
+					code: 22,
+					title: "全球主要经济体贸易出口增速"
 				}
 				]
 			}
@@ -141,18 +223,13 @@
 			initReportDate: function () {
 				let my = new Date();
 				return my.getFullYear()+''+my.getMonth()+my.getDate();
-			},
-			closeTapTarget: function () {
-				jQuery('.tap-target').tapTarget('close');
-			},
-			toggle: function () {
-				jQuery('.tap-target').tapTarget('open');
 			}
 		},
 		mounted () {
 			this.$nextTick(function () {
 				jQuery(document).ready(function(){
 					jQuery('.collapsible').collapsible();
+					jQuery('.modal').modal();
 				});
 			})
 			
@@ -167,7 +244,7 @@
 		color: black;
 	}
 	#graphSelectArea {
-		text-align: center;
+		text-align: left;
 		color: black;
 	}
 </style>
