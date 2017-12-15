6bc16be5acf83a6db993647183378e9fe8cbac23
yicai9329 <18081878178@163.com>
2017-12-15 17:38:23 +0800

    comment

--- src/components/docItemSelect.vue
+++ src/components/docItemSelect.vue
@@ -3,11 +3,11 @@
 		<ul class="collapsible" data-collapsible="accordion">
 			<template v-for="(captionItem, indexI) in captionItems">
 				<li :key="captionItem.captionI">
-					<div class="collapsible-header"><button @click="captionIUpOne(indexI)">up</button><button @click="captionIDownOne(indexI)">down</button><i class="material-icons">place</i>{{captionItem.captionI}}<button @click="removeCaptionItemI(indexI)">X</button></div>
+					<button @click="captionIUpOne(indexI)">up</button><button @click="captionIDownOne(indexI)">down</button><div class="collapsible-header"><i class="material-icons">place</i>{{captionItem.captionI}}<button @click="removeCaptionItemI(indexI)">X</button></div>
 					<div class="collapsible-body">
 						<template v-for="(subTitle, indexII) in captionItem.children">
 							<ul class="collection" :key="subTitle.captionII">
-								<li class="collection-item"><button @click="captionIIUpOne(indexI,indexII)">up</button><button @click="captionIIDownOne(indexI,indexII)">down</button><a href="#!">{{subTitle.captionII}}<button @click="removeCaptionItemII(indexI,indexII)">X</button></a></li>
+								<li class="collection-item"><button @click="captionIIUpOne([indexI,indexII])">up</button><button @click="captionIIDownOne([indexI,indexII])">down</button><a href="#!">{{subTitle.captionII}}<button @click="removeCaptionItemII([indexI,indexII])">X</button></a></li>
 							</ul>
 						</template>
 					</div>
@@ -15,12 +15,21 @@
 			</template>
 		</ul>
 		<div class="row" id="addButtonForCaptionOne">
-			<span class="deep-purple-text darken-4">增加一级标题</span>&nbsp;<a class="btn-floating btn-small waves-effect waves-light red"><i class="material-icons">add</i></a>
+			<span class="deep-purple-text darken-4">增加一级标题</span>&nbsp;<a class="btn-floating btn-small waves-effect waves-light red modal-trigger" href="#modal33"><i class="material-icons">add</i></a>
 			<span class="deep-purple-text darken-4">减去一级标题</span>&nbsp;<a class="btn-floating btn-small waves-effect waves-light red"><i class="material-icons">cancel</i></a>
 		</div>
 		<div class="row" id="addButtonForCaptionTwo">
 			<span class="deep-purple-text darken-4">增加二级标题</span>&nbsp;<a class="btn-floating btn-small waves-effect waves-light red"><i class="material-icons">add</i></a>
 			<span class="deep-purple-text darken-4">减少二级标题</span>&nbsp;<a class="btn-floating btn-small waves-effect waves-light red"><i class="material-icons">cancel</i></a>
+		</div>
+		<div id="modal33" class="modal">
+			<div class="modal-content">
+				<input placeholder="一级标题" id="new_captionI" type="text" v-model="newCaptionI">
+				<label for="new_captionI">新增一级标题</label>
+			</div>
+			<div class="modal-footer">
+				<a class="modal-action modal-close waves-effect waves-green btn-flat" @click="addCaptionI">确认</a>
+			</div>
 		</div>
 	</div>
 </template>
@@ -35,7 +44,8 @@
 		},
 		computed: {
 			...mapState({
-				captionItems: state => state.docItemSelect.captionItems
+				captionItems: state => state.docItemSelect.captionItems,
+				newCaptionI: state => state.docItemSelect.newCaptionI
 			}),
 		},
 		methods: {
@@ -45,7 +55,8 @@
 				'captionIUpOne',
 				'captionIDownOne',
 				'captionIIUpOne',
-				'captionIIDownOne'
+				'captionIIDownOne',
+				'addCaptionI'
 				]),
 		},
 		props: {
--- src/store/modules/docItemSelect.js
+++ src/store/modules/docItemSelect.js
@@ -1,18 +1,19 @@
 const state = {
 	captionItems:  [
-	{ captionI: "报告摘要" , reportID: this.reportID,
+	{ captionI: "报告摘要" , 
 	children:[ {captionII: "全球经济形势的整体判断"}, {captionII: "国内经济形势的整体判断"} ]
 },                                                                                                              
-{ captionI: "全球经济发展趋势", reportID: this.reportID,
+{ captionI: "全球经济发展趋势", 
 children:[{captionII: "全球经济保持回暖态势，美欧中制造业景气达近年高点"}, {captionII: "全球贸易出口增势强劲，贸易失衡依然严重"}, {captionII: "前三季美元疲软，欧、日元和人民币走势强劲"}]
 },
-{captionI: "产业经济分析", reportID: this.reportID,
+{captionI: "产业经济分析", 
 children:[{captionII: "大宗商品价格冲高回落，钢铁价格领头"},{captionII: "钢铁效益明显回升，通信、计算机、仪器仪表与汽车产业领增"}]
 },
-{captionI: "结论及建议", reportID: this.reportID, children:[
+{captionI: "结论及建议",  children:[
 {captionII: "整体结论及趋势预测"}
 ]}                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-]
+],
+newCaptionI: ""
 }
 
 var swapItems = function(arr, index1, index2) {
@@ -27,22 +28,23 @@ const actions = {
 	removeCaptionItemI({commit}, indexI){
 		commit('removeCaptionItemI', {indexI})
 	},
-	removeCaptionItemII({commit}, indexI, indexII){
-		console.log("INDEX II IS " + indexII)
-		commit('removeCaptionItemII', {indexI, indexII})
+	removeCaptionItemII({commit}, [indexI, indexII]){
+		commit('removeCaptionItemII', [indexI, indexII])
 	},
 	captionIUpOne({commit}, indexI){
 		commit('captionIUpOne', {indexI})
 	},
-	captionIIUpOne({commit}, indexI,indexII){
-		console.log("INDEX II IS " + indexII)
-		commit('captionIIUpOne', {indexI,indexII})
+	captionIIUpOne({commit}, [indexI,indexII]){
+		commit('captionIIUpOne', [indexI,indexII])
 	},
 	captionIDownOne({commit}, indexI){
 		commit('captionIDownOne', {indexI})
 	},
-	captionIIDownOne({commit}, indexI, indexII){
-		commit('captionIIDownOne', {indexI, indexII})
+	captionIIDownOne({commit}, [indexI, indexII]){
+		commit('captionIIDownOne', [indexI, indexII])
+	},
+	addCaptionI({commit}){
+		commit('addCaptionI')
 	}
 }
 
@@ -50,35 +52,40 @@ const mutations = {
 	removeCaptionItemI(state, {indexI}){
 		state.captionItems.splice(indexI, 1)
 	},
-	removeCaptionItemII(state, {indexI, indexII}){
-		console.log("INDEX II IS " + indexII)
+	removeCaptionItemII(state, [indexI, indexII]){
 		state.captionItems[indexI].children.splice(indexII,1)
 	},
 	captionIUpOne(state, {indexI}){
 		if(indexI === 0){
-           return;
+			return;
 		}
 		swapItems(state.captionItems, indexI, indexI-1);
 	},
-	captionIIUpOne(state, {indexI,indexII}){
-		console.log("INDEX II IS " + indexII)
+	captionIIUpOne(state, [indexI,indexII]){
 		if(indexII === 0) {
 			return;
 		}
 		swapItems(state.captionItems[indexI].children, indexII, indexII-1);
 	},
-    captionIDownOne(state, {indexI}){
-    	if(indexI === state.captionItems.length-1){
-    		return;
-    	}
-    	swapItems(state.captionItems, indexI, indexI+1);
-    },
-    captionIIDownOne(state, {indexI,indexII}){
-    	if(indexII === state.captionItems[indexI].length-1){
-    		return;
-    	}
-    	swapItems(state.captionItems[indexI].children, indexII, indexII+1);
-    }
+	captionIDownOne(state, {indexI}){
+		if(indexI === state.captionItems.length-1){
+			return;
+		}
+		swapItems(state.captionItems, indexI, indexI+1);
+	},
+	captionIIDownOne(state, [indexI,indexII]){
+		if(indexII === state.captionItems[indexI].length-1){
+			return;
+		}
+		swapItems(state.captionItems[indexI].children, indexII, indexII+1);
+	},
+	addCaptionI(state){
+		console.log("The new title is " + state.newCaptionI);
+		let newTitle = state.newCaptionI;
+		let newCaptionItem = {captionI: newTitle, children:[]};
+		state.captionItems.push(newCaptionItem);
+		// state.newCaptionI =  "";
+	}
 }
 
 
